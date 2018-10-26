c04eb83f3ef58b8b4246a66dd227d5f061d26e96
Taylor Monacelli <taylormonacelli@gmail.com>
2018-10-25 22:57:48 -0700

    Add content

--- filesize_from_stdin/filesize_from_stdin.py
+++ filesize_from_stdin/filesize_from_stdin.py
@@ -1,3 +1,21 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+import operator
+import sys
+import humanize
+import os
+from pathlib import Path
+
+
+    flist = {}
+
+    for line in sys.stdin.readlines():
+        path = Path(line.strip()).resolve()
+        if os.path.exists(path):
+            flist[path] = os.stat(path).st_size
+
+    # sort by size
+    for path, size in sorted(flist.items(), key=operator.itemgetter(1)):
+        print(f'{humanize.naturalsize(size, gnu=True)} {path}')
