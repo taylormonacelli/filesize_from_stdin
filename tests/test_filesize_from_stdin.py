985b7010955685cc237b08449d8060d97e7c72e7
Taylor Monacelli <taylormonacelli@gmail.com>
2018-10-27 14:13:16 -0700

    help_result

--- tests/test_filesize_from_stdin.py
+++ tests/test_filesize_from_stdin.py
@@ -89,5 +89,5 @@ def test_commandline_help():
     """Test the CLI."""
     runner = CliRunner()
     help_result = runner.invoke(cli.main, ['--help'])
-    assert help_int(result.exit_code) == 0
+    assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
