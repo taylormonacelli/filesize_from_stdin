dc0d0f0ef4c986d800be942fc6f308a71f774fef
Taylor Monacelli <taylormonacelli@gmail.com>
2018-10-27 16:26:44 -0700

    Revert "fix python 27"
    This reverts commit d20ad4234502607add71039d5ba4f93b7ef1a784.

--- filesize_from_stdin/cli.py
+++ filesize_from_stdin/cli.py
@@ -3,10 +3,8 @@
 """Console script for filesize_from_stdin."""
 import sys
 import click
-import os.path
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-
 from filesize_from_stdin.filesize_from_stdin import doit
+
 
 @click.command()
 def main():
