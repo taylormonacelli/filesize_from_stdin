e88b3e2fd46fc5198d84f6056b50b01f90e7c2df
Taylor Monacelli <taylormonacelli@gmail.com>
2018-10-27 17:52:32 -0700

    warning: duplicating, return to this!

--- setup.py
+++ setup.py
@@ -11,7 +11,11 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = [
+    'Click>=6.0',
+    'pathlib',
+    'humanize',
+]
 
 setup_requirements = ['pytest-runner', ]
 
