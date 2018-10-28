27594b1d0c718489ec490a495da3ba3b78ade0c1
Taylor Monacelli <taylormonacelli@gmail.com>
2018-10-28 16:14:28 -0700

    Bump version: 1.0.1 → 1.0.2

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/taylormonacelli/filesize_from_stdin',
-    version='1.0.1',
+    version='1.0.2',
     zip_safe=False,
 )
