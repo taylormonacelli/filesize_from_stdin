58439600a56dcc93758e27ba1a4ec74f3b124332
Taylor Monacelli <taylormonacelli@gmail.com>
2018-10-29 11:03:15 -0700

    t1

--- filesize_from_stdin/cli.py
+++ filesize_from_stdin/cli.py
@@ -3,13 +3,13 @@
 """Console script for filesize_from_stdin."""
 import sys
 import click
-from .filesize_from_stdin import display_friendly, get_file_list
+from .filesize_from_stdin import display_friendly, get_file_sizes
 
 
 @click.command()
 def main(args=None):
     """Console script for filesize_from_stdin."""
-    display_friendly(get_file_list(sys.stdin))
+    display_friendly(get_file_sizes(sys.stdin))
 
 
 if __name__ == "__main__":
--- filesize_from_stdin/filesize_from_stdin.py
+++ filesize_from_stdin/filesize_from_stdin.py
@@ -4,26 +4,39 @@
 
 import operator
 import sys
+import io
 from pathlib import Path
 import humanize
 
 
 def get_file_list(source=sys.stdin):
+    """Get list of files form stdin"""
+    lst = []
+
+    if isinstance(source, io.TextIOWrapper):
+        # if stdin is empty
+        if source.isatty():
+            return lst
+
+    for line in source:
+        path = Path(line.strip())
+        if path.exists():
+            lst.append(path.resolve())
+
+    return lst
+
+
+def get_file_sizes(lst):
     """
     Given a list of paths separated by newline, output
     the list of files and their size in bytes sorted by size.
     """
 
-    # if stdin is empty
-    if sys.stdin.isatty():
-        return {}
-
     dct = {}
 
-    for line in source:
-        path = Path(line.strip())
-        if path.exists():
-            dct[str(path.resolve())] = path.stat().st_size
+    lst = get_file_list()
+    for path in lst:
+        dct[str(path)] = path.stat().st_size
 
     return dct
 
