da4dac923228a9d4966294785aecd46129c4e2f7
Taylor Monacelli <taylormonacelli@gmail.com>
2018-10-29 14:14:14 -0700

    Revert "t1"
    This reverts commit 58439600a56dcc93758e27ba1a4ec74f3b124332.

--- filesize_from_stdin/cli.py
+++ filesize_from_stdin/cli.py
@@ -3,13 +3,13 @@
 """Console script for filesize_from_stdin."""
 import sys
 import click
-from .filesize_from_stdin import display_friendly, get_file_sizes
+from .filesize_from_stdin import display_friendly, get_file_list
 
 
 @click.command()
 def main(args=None):
     """Console script for filesize_from_stdin."""
-    display_friendly(get_file_sizes(sys.stdin))
+    display_friendly(get_file_list(sys.stdin))
 
 
 if __name__ == "__main__":
