7395fbb78083ef48d966524e60ccf45b8b62aafb
Taylor Monacelli <taylormonacelli@gmail.com>
2018-10-29 21:48:12 -0700

    [skip ci] yada-yada will rebase when working

--- filesize_from_stdin/filesize_from_stdin.py
+++ filesize_from_stdin/filesize_from_stdin.py
@@ -14,7 +14,7 @@ def get_file_list(source=sys.stdin):
     the list of files and their size in bytes sorted by size.
     """
 
-    # if stdin is empty
+    # prevent blocking if stdin is empty
     if sys.stdin.isatty():
         return {}
 
