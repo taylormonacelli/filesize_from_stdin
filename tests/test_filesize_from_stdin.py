86ba961808eb1ade82b1f75923526caf2c83b28f
Taylor Monacelli <taylormonacelli@gmail.com>
2018-10-30 16:53:28 -0700

    Add tests

--- tests/test_filesize_from_stdin.py
+++ tests/test_filesize_from_stdin.py
@@ -21,18 +21,72 @@ def response():
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_non_empty_file(tmpdir):
+    """Test file with single word in it."""
+    runner = CliRunner()
+    path = tmpdir.join("hello.txt")
+    path.write("content")
+    assert path.read() == "content"
+    result = runner.invoke(cli.main, input=str(path))
+    assert result.exit_code == 0
+    assert '7B {}'.format(path) in result.output
+
+
+def test_file_with_space(tmpdir):
+    """Test file name with spaces in it."""
+    runner = CliRunner()
+    path = tmpdir.join("hel lo.txt")
+    path.write("content")
+    assert path.read() == "content"
+    result = runner.invoke(cli.main, input='{}'.format(path))
+    assert result.exit_code == 0
+    assert '7B {}'.format(path) in result.output
 
 
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'filesize_from_stdin.cli.main' in result.output
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
+
+
+def test_file_with_quote_in_name(tmpdir):
+    """Test the CLI."""
+    runner = CliRunner()
+    path = tmpdir.join("hello'.txt")
+    path.write("content")
+    assert path.read() == "content"
+    result = runner.invoke(cli.main, input=str(path))
+    assert result.exit_code == 0
+    assert '7B {}'.format(path) in result.output
+
+
+def test_empty_file(tmpdir):
+    """Test file with no contents."""
+    runner = CliRunner()
+    path = tmpdir.join("hello1.txt")
+    path.write("")
+    assert path.read() == ""
+    result = runner.invoke(cli.main, input=str(path))
+    assert result.exit_code == 0
+    assert '0B {}'.format(path) in result.output
+
+
+def test_file_does_not_exist(tmpdir):
+    """Test non existant files."""
+    runner = CliRunner()
+    sub = tmpdir.mkdir("sub")
+    path = sub.join("non_existant_file.txt")
+    assert not sub.listdir()
+    result = runner.invoke(cli.main, input=str(path))
+    assert result.exit_code == 0
+    assert result.output == ''
+
+
+def test_no_stdin_given():
+    """Here's what happens if you don't pass input."""
+    runner = CliRunner()
+    result = runner.invoke(cli.main)
+    assert result.exit_code == 0
+    assert result.output == ''
