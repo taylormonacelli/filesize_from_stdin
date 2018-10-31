de7eecc198443d78ee77ff6bec54dd2ed4c5c763
Taylor Monacelli <taylormonacelli@gmail.com>
2018-10-31 13:35:41 -0700

    wondering

--- filesize_from_stdin/cli.py
+++ filesize_from_stdin/cli.py
@@ -10,12 +10,7 @@ from .filesize_from_stdin import display_friendly, get_file_list
 @click.argument('filenames', nargs=-1, type=click.Path())
 def main(filenames):
     """Console script for filesize_from_stdin."""
-    if filenames:
-        source = filenames[0]
-    else:
-        source = sys.stdin
-
-    display_friendly(get_file_list(source))
+    display_friendly(get_file_list(filenames))
     return 0
 
 
